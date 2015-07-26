2ed655d5f3c6b1c8e8316b338f45458833d1ef29
Dominic Butler <swervo@papersnail.co.uk>
2015-07-26 18:47:48 +0100

    Initial

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var UI = require('ui');
+
+// Create a Card with title and subtitle
+var card = new UI.Card({
+  title:'Weather',
+  subtitle:'Fetching...'
+});
+
+// Display the Card
+card.show();
